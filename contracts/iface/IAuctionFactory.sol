c5a0ec5a1a94d29e73c02b74c19c2d77c9d03d6a
Daniel Wang <daniel@loopring.org>
2019-04-27 20:04:28 -0400

    rename salt function

--- contracts/iface/IAuctionFactory.sol
+++ contracts/iface/IAuctionFactory.sol
@@ -14,7 +14,7 @@
   See the License for the specific language governing permissions and
   limitations under the License.
 */
-pragma solidity 0.5.5;
+pragma solidity 0.5.7;
 pragma experimental ABIEncoderV2;
 
 import "./IAuctionData.sol";
